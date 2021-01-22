1ca02758f206194d870f6e319bb1d058290f0429
Tom Chengich <tomcengich@gmail.com>
2021-01-22 19:28:28 +0200

    fixed move semantics.

--- ps2asm/main.cpp
+++ ps2asm/main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include "Disassembler.h"
 
+
 int main(int argc, char* argv[]) {
 	if (argc != 2) {
 		std::cerr << "No input file given.\n" << "Usage: ps2asm.exe filename" << std::endl;
 		return 1;
 	}
-	Disassembler disassembler{ std::move(std::string(argv[1])) };
+	Disassembler disassembler{ std::string(argv[1]) };
 }
