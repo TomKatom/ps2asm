5407b3de87783fd2d691104642df3dac72c7af62
Tom Chengich <tomcengich@gmail.com>
2021-01-22 19:28:02 +0200

    Basic main structure implemented.

---
+++ ps2asm/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "Disassembler.h"
+
+int main(int argc, char* argv[]) {
+	if (argc != 2) {
+		std::cerr << "No input file given.\n" << "Usage: ps2asm.exe filename" << std::endl;
+		return 1;
+	}
+	Disassembler disassembler{ std::move(std::string(argv[1])) };
+}
